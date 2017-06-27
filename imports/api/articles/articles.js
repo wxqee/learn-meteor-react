e6f60f6c8aa32f8988e61e02d082357852b7dd0f
Wang Xiaoqiang <xq21@vip.qq.com>
2017-06-28 00:11:47 +0800

    methods and validators

--- imports/api/articles/articles.js
+++ imports/api/articles/articles.js
@@ -1,3 +1,26 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Articles = new Mongo.Collection('articles');
+
+Articles.schema = new SimpleSchema({
+  subject: {
+    type: String,
+    label: 'Subject',
+  },
+  content: {
+    type: String,
+    label: 'Content',
+  },
+});
+
+Articles.schema.addValidator(() => {
+  if (this.key === 'subject' && this.value.length < 6) {
+    return 'Subject at least 6 characters.';
+  } else if (this.key === 'content' && this.value.length < 30) {
+    return 'Content at least 30 characters.';
+  }
+});
+
+Articles.helpers({
+
+});
---
+++ imports/api/articles/methods.js
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+import { Articles } from './articles.js';
+
+Meteor.methods({
+  'articles.add'(formData) {
+    Articles.schema.validate(formData)
+
+    return Articles.insert({
+      ...formData,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  },
+  'articles.remove'(_id) {
+    return Articles.remove(_id);
+  },
+  'articles.get'(filterData) {
+    return Articles.find({}, filterData);
+  },
+});
